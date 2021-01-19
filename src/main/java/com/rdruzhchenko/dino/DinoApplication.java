128080c5690f5827ab0344313d6db9fc8e662e25
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2021-01-19 13:40:19 +0200

    Initial commit

---
+++ src/main/java/com/rdruzhchenko/dino/DinoApplication.java
@@ -0,0 +1,13 @@
+package com.rdruzhchenko.dino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdruzhchenko/dino/DinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdruzhchenko.dino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
